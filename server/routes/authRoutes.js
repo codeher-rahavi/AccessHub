eb71fcfbf9e8cac4467bef1eb970f80d43893f1f
codeher-rahavi <rahaviganeshan529@gmail.com>
2026-09-14 20:38:47 +0530

    security JWT implementation

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { signUp, signIn } = require("../controllers/authControllers");
+const { signUp, signIn } = require("../controllers/authController");
+const verifyToken = require("../middleware/authMiddleware");
 
-// POST /api/signup
+// Public routes
 router.post("/signup", signUp);
-
-// POST /api/login
 router.post("/login", signIn);
+
+// Protected route
+router.get("/protected", verifyToken, (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "You have access to this protected route.",
+        user: req.user
+    });
+});
 
 module.exports = router;
