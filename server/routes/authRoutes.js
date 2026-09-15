f6540ed18a56180caf4764ef10b30b24036c2a14
codeher-rahavi <rahaviganeshan529@gmail.com>
2026-09-15 09:00:31 +0530

    signIn and signUp basic completed

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signUp, signIn } = require("../controllers/authController");
+const { signUp, signIn } = require("../controllers/authControllers");
 const verifyToken = require("../middleware/authMiddleware");
 
 // Public routes
